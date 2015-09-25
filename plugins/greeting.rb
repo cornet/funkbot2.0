5657fd325f5938bae3dcd997f94dff516f932a42
cornet <cornet@sheepy.org>
2015-09-25 18:58:47 +0100

    Remove leading space from greeting

--- plugins/greeting.rb
+++ plugins/greeting.rb
@@ -6,8 +6,7 @@ class Greeting
   match /^(hello|hi|ello|morning|afternoon|evening)(\W+.*|$)/i, use_prefix: false
 
   def execute(m)
-    greetings = ['Hi', 'Hello',' Hola!']
+    greetings = ['Hi','Hello','Hola!']
     m.reply "#{greetings.sample} #{m.user.nick}"
   end
-
 end
